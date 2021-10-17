578a7b82b3d8a8d1f31ee1154f1d7acf8a9951bf
Eva Anette Johansen <evaajo@stud.ntnu.no>
2021-10-17 17:11:33 +0000

    (#50) made a test to check if controller is null

--- studdyBuddy/ui/src/test/java/ui/StuddyBuddyControllerTest.java
+++ studdyBuddy/ui/src/test/java/ui/StuddyBuddyControllerTest.java
@@ -13,24 +13,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 //import javafx.scene.control.ListView;
 import javafx.stage.Stage;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 //import static org.junit.jupiter.api.Assertions.assertTrue;
 //import static org.junit.jupiter.api.Assertions.fail;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 //import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.testfx.framework.junit5.ApplicationTest;
 //import org.testfx.util.WaitForAsyncUtils;
 //import StuddyBuddy.core.AbstractTodoList;
 //import StuddyBuddy.core.StuddyBuddyRegister;
-//import StuddyBuddy.core.StuddyBuddy;
+import core.StuddyBuddy;
 //import StuddyBuddy.core.TodoModel;
 //import StuddyBuddy.core.TodoSettings.TodoItemsSortOrder;
 //import StuddyBuddy.json.TodoPersistence;
 
-import core.StuddyBuddy;
 import core.StuddyBuddyRegistration;
 
 public class StuddyBuddyControllerTest extends ApplicationTest {
@@ -63,7 +62,12 @@ public class StuddyBuddyControllerTest extends ApplicationTest {
       studdyBuddy.addRegistration(reg2);
       studdyBuddy.addRegistration(reg3);
     }
-  
+    
+    @Test
+    public void testController_studdyBuddy() {
+      assertNotNull(this.controller);
+      assertNotNull(this.studdyBuddy);
+    }
 
     
 }
