05677f5322f325f1b0e36f56f678d09b5fd7afda
Tuva Langedal Djupvik <tuvald@stud.ntnu.no>
2021-11-02 12:50:27 +0000

    Made studdybuddies test and added comments to tests in core

--- studdyBuddy/core/src/test/java/core/StuddyBuddiesTest.java
+++ studdyBuddy/core/src/test/java/core/StuddyBuddiesTest.java
@@ -4,15 +4,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Class for testing StuddyBuddies class.
+ */
 public class StuddyBuddiesTest {
 
     private StuddyBuddies buddies;
 
+    /**
+     * Setup for each test. 
+     */
     @BeforeEach
     public void setup() {
         buddies = new StuddyBuddies();
     }
 
+    /**
+     * Method for testing getStuddyBuddy method. 
+     */
     @Test
     public void testGetStuddyBuddy() {
         assertNull(buddies.getStuddyBuddy("Tuva"));
@@ -23,7 +32,8 @@ public class StuddyBuddiesTest {
     }
 
     /**
-     * This method tests checkIfStuddyBuddyExists indirectly because it is used in addStuddyBuddy.
+     * Method for testing addStuddyBuddy method.
+     * It indirectly tests if checkIfStuddyBuddyExists works, because it is used in addStuddyBuddy.
      */
     @Test
     public void testAddStuddyBuddy() {
@@ -34,8 +44,30 @@ public class StuddyBuddiesTest {
         assertThrows(IllegalArgumentException.class, () -> buddies.addStuddyBuddy(buddy));
     }
 
+    /**
+     * Method for testing addStuddyBuddies method. 
+     */
     @Test
     public void testAddStuddyBuddies() {
-        
+        StuddyBuddy buddy = new StuddyBuddy();
+        buddy.setName("Tuva");
+        buddies.addStuddyBuddy(buddy);
+        StuddyBuddy buddy2 = new StuddyBuddy();
+        buddy2.setName("Selma");
+        buddies.addStuddyBuddies(buddy2);
+        assertTrue(buddies.getStuddyBuddies().contains(buddy));
+        assertTrue(buddies.getStuddyBuddies().contains(buddy2));
+        assertThrows(IllegalArgumentException.class, () -> buddies.addStuddyBuddies(buddy, buddy2));
+    }
+
+    /**
+     * Method for testing removeStuddyBuddy method. 
+     */
+    @Test
+    public void testRemoveStuddyBuddy() {
+        StuddyBuddy buddy = new StuddyBuddy();
+        buddies.addStuddyBuddy(buddy);
+        buddies.removeStuddyBuddy(buddy);
+        assertTrue(!buddies.getStuddyBuddies().contains(buddy));
     }
 }
