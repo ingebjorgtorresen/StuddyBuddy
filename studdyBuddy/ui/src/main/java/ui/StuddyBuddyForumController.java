f6d24589d9aa35363295579f4cfc43ed348e3b6a
Ingebjørg Semb Tørresen <ingebsto@stud.ntnu.no>
2021-11-10 10:07:24 +0000

    (#76) Created controller and fxml for forum-page

---
+++ studdyBuddy/ui/src/main/java/ui/StuddyBuddyForumController.java
@@ -0,0 +1,116 @@
+package ui;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import core.StuddyBuddies;
+import core.StuddyBuddy;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import json.StuddyBuddiesPersistence;
+
+public class StuddyBuddyForumController {
+
+    private StuddyBuddy studdyBuddy;
+    private StuddyBuddies studdyBuddies;
+    private StuddyBuddiesPersistence persistence = new StuddyBuddiesPersistence();
+    private String registrationsFileName = "/registrations.json";
+
+    @FXML
+    private ImageView studdyBuddyLogo;
+
+    @FXML
+    private ImageView userIcon;
+
+    @FXML
+    private Label username;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    private Button addRegistration;
+    
+    @FXML
+    private Label studdyBuddyUser;
+
+    @FXML
+    private Label registrationsTitle;
+
+    @FXML
+    private Label allRegistrationsText;
+
+
+    public void initialize() {
+        // usikker på om det er riktig å opprette et StuddyBuddies objekt her
+        studdyBuddies = new StuddyBuddies();
+        createForum();
+    }
+
+    private void createForum() {
+        setStuddyBuddyFromLogin();
+        username.setText(studdyBuddy.getName());
+    }
+
+    private void setStuddyBuddy(StuddyBuddy studdyBuddy) {
+        this.studdyBuddy = studdyBuddy;
+    }
+
+    public void setStuddyBuddyFromLogin() {
+        setStuddyBuddy(studdyBuddy);
+    }
+
+    @FXML
+    public void handleAddRegistration() {
+        
+        try {
+
+            URL fxmlFile = getClass().getResource("StuddyBuddyRegistration.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmlFile);
+            Parent parent = (Parent) loader.load();
+            Stage registrationStage = new Stage();
+            registrationStage.setTitle("Registration");
+            registrationStage.setScene(new Scene(parent));
+            registrationStage.show();
+            Stage thisStage = (Stage) username.getScene().getWindow();
+            thisStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void handleLogOut(){
+        // TODO
+    }
+
+    public StuddyBuddies getRegistratedStuddyBuddies(){
+        StuddyBuddies registeredBuddies = null;
+        try (Reader reader = new FileReader(System.getProperty("user.home") + registrationsFileName, StandardCharsets.UTF_8)) {
+                registeredBuddies = persistence.readStuddyBuddies(reader);
+        } catch (Exception e) {
+            System.err.println("Couldn´t read from file");
+            e.printStackTrace();
+        }
+        return registeredBuddies;
+    }
+/**
+    public void displayRegistrations() {
+        StuddyBuddies registeredBuddies = getRegistratedStuddyBuddies();
+        if (registeredBuddies != null){
+            allRegistrationsText.setText(studdyBuddies.getStuddyBuddies().toString());
+        }
+    }*/
+
+
+}
