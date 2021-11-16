0eeb3155f1116dbd0ef42a593a8850bc7e82bc3a
Selma Hidle <selmahi@stud.ntnu.no>
2021-11-16 17:27:55 +0000

    Fixed the rest of the directory structure

--- studdybuddy/restserver/src/main/java/studdybuddy/module-info.java
+++ studdybuddy/restserver/src/main/java/studdybuddy/module-info.java
@@ -1,10 +1,16 @@
 module studdybuddy.restserver {
-    requires com.fasterxml.jackson.databind;
+    requires transitive com.fasterxml.jackson.databind;
 
     requires spring.web;
+    requires spring.core;
     requires spring.beans;
     requires spring.boot;
+    requires spring.context;
     requires spring.boot.autoconfigure;
 
+    requires org.slf4j;
+
     requires studdybuddy.core;
+
+    opens studdybuddy.restserver to spring.beans, spring.context, spring.web;
 }
