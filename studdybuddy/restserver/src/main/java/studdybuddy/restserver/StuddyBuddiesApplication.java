0eb32fa141b77b95aead8fa55da499dfdd98b015
Eva Anette Johansen <evaajo@stud.ntnu.no>
2021-11-17 15:40:49 +0000

    (#92) add method for welcomeService

--- studdybuddy/restserver/src/main/java/studdybuddy/restserver/StuddyBuddiesApplication.java
+++ studdybuddy/restserver/src/main/java/studdybuddy/restserver/StuddyBuddiesApplication.java
@@ -3,14 +3,23 @@ package studdybuddy.restserver;
 import com.fasterxml.jackson.databind.Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import studdybuddy.json.*;
 
-
+@RestController
 @SpringBootApplication
 public class StuddyBuddiesApplication {
 
-@Bean
+  private WelcomeService welcomeService = new WelcomeService();
+  
+  @GetMapping("/")
+  public String home() {
+    return this.welcomeService.getWelcomeText();
+  }
+
+  @Bean
   public Module objectMapperModule() {
     return new StuddyModule();
   }
