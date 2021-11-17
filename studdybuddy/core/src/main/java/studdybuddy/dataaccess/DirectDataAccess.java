9715199d71e74d1296741d05dd5304cfaac16558
Eva Anette Johansen <evaajo@stud.ntnu.no>
2021-11-17 15:39:34 +0000

    (#92) changed constructor, deleted unused methods

--- studdybuddy/core/src/main/java/studdybuddy/dataaccess/DirectDataAccess.java
+++ studdybuddy/core/src/main/java/studdybuddy/dataaccess/DirectDataAccess.java
@@ -1,13 +1,5 @@
 package studdybuddy.dataaccess;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.Writer;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
 import studdybuddy.core.*;
 import studdybuddy.json.StuddyBuddiesPersistence;
 
@@ -18,79 +10,14 @@ import studdybuddy.json.StuddyBuddiesPersistence;
 
 public class DirectDataAccess implements DataAccess {
 
-    private StuddyBuddies buddies;
-    private String studdybuddiesFilename = "testStuddyBuddies.json";
-    private Boolean persistence = true;
+    private final StuddyBuddies buddies;
     private StuddyBuddiesPersistence buddiesPersistence = new StuddyBuddiesPersistence();
 
     /**
-     * Constructor that creates an instance that reads from a json-file.
+     * Constructor that creates an instance of StuddyBuddies
      */
-    public DirectDataAccess() {
-        buddies = readStuddyBuddies();
-    }
-
-    /**
-     * Constructor that creates an instance that reads from the json.file specified
-     * as a parameter.
-     * 
-     * @param file the filename of the json file to write to
-     */
-    public DirectDataAccess(String file) {
-        studdybuddiesFilename = file;
-        buddies = readStuddyBuddies();
-    }
-
-    /**
-     * Method for reading from json-file with reasource from field:
-     * studdybuddiesFilename
-     * 
-     * @return a StuddyBuddies object
-     */
-    public StuddyBuddies readStuddyBuddies() {
-        if (studdybuddiesFilename != null) {
-            try {
-                URL url = getClass().getResource(studdybuddiesFilename);
-                Reader reader = new InputStreamReader(url.openStream(), StandardCharsets.UTF_8);
-                StuddyBuddies buddies = buddiesPersistence.readStuddyBuddies(reader);
-                this.buddies = buddies;
-                return buddies;
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new IllegalStateException("Unable to read from file: " + studdybuddiesFilename);
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Method for writing a StuddyBuddy object serialized as a json-string to file
-     * in the resource file.
-     * 
-     * @param buddies StuddyBuddies object to save
-     */
-    public void writeStuddyBuddies(StuddyBuddies buddies) {
-        if (studdybuddiesFilename != null) {
-            try {
-                URL url = getClass().getResource(studdybuddiesFilename);
-                Writer writer = new FileWriter(url.getFile(), StandardCharsets.UTF_8);
-                buddiesPersistence.writeStuddyBuddies(buddies, writer);
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            } catch (IOException | RuntimeException e) {
-                throw new IllegalStateException("Could not write to file: " + studdybuddiesFilename);
-            }
-        }
-    }
-
-    /**
-     * Method that writes buddies field to the file.
-     */
-    public void writeStuddyBuddies() {
-        writeStuddyBuddies(this.buddies);
+    public DirectDataAccess(StuddyBuddies buddies) {
+        this.buddies = buddies;
     }
 
     @Override
@@ -105,72 +32,18 @@ public class DirectDataAccess implements DataAccess {
     @Override
     public String getStuddyBuddyPasswordByName(String name) {
         return getStuddyBuddyByName(name).getPassword();
+    }  
+
+    public void addStuddyBuddy(StuddyBuddy buddy) {
+        buddies.addStuddyBuddy(buddy);
     }
 
-    @Override
-    public void putStuddyBuddy(StuddyBuddy buddy) {
-        this.readStuddyBuddies();
+    public void autoSaveStuddyBuddies() {
         try {
-            this.buddies.addStuddyBuddy(buddy);
-        } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException(
-                    "User already exist in the server, and the user can therefore not be created");
-        }
-        // This is for testing
-        if (persistence) {
-            writeStuddyBuddies();
+            buddiesPersistence.saveStuddyBuddies(buddies);
+        } catch (Exception e) {
+            System.err.println("Could not save StuddyBuddies: " + e.getMessage());
         }
     }
 
-    @Override
-    public void postStuddyBuddy(StuddyBuddy buddy) {
-        this.readStuddyBuddies();
-        try {
-            this.buddies.getStuddyBuddy(buddy.getName()).updateStuddyBuddyObject(buddy);
-            ;
-        } catch (NullPointerException e) {
-            throw new IllegalArgumentException(
-                    "User does not exist in the server, and the user can therefore not be updated.");
-        }
-        // This is for testing
-        if (persistence) {
-            writeStuddyBuddies();
-        }
-
-    }
-
-    /**
-     * Method for setting StuddyBuddies objects.
-     * 
-     * @param buddies StuddyBuddies object to set
-     */
-    public void setStuddyBuddies(StuddyBuddies buddies) {
-        this.buddies = buddies;
-    }
-
-    /**
-     * Method for setting studdybuddiesFilename
-     * 
-     * @param filename the filename to be set
-     */
-    public void setFilename(String filename) {
-        this.studdybuddiesFilename = filename;
-    }
-
-    /**
-     * Method for getting the filename
-     * 
-     * @return the filename
-     */
-    public String getFilename() {
-        return studdybuddiesFilename;
-    }
-
-    /**
-     * Method for writing a empty StuddyBuddy object to the json-file. This is used
-     * for testing when one wants to clear the document before testing.
-     */
-    public void writeEmptyStuddyBuddyToDocument() {
-        writeStuddyBuddies(new StuddyBuddies());
-    }
 }
