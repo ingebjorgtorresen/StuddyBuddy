e9108ebc236f8151a356f9b0cb48c3b24737b886
Selma Hidle <selmahi@stud.ntnu.no>
2021-11-22 18:10:29 +0000

    (#96) Removed unused fileds and methods

--- studdybuddy/ui/src/main/java/studdybuddy/ui/AppController.java
+++ studdybuddy/ui/src/main/java/studdybuddy/ui/AppController.java
@@ -1,14 +1,8 @@
 package studdybuddy.ui;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -16,8 +10,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import studdybuddy.core.StuddyBuddies;
-import studdybuddy.core.StuddyBuddy;
-import studdybuddy.core.StuddyBuddyRegistration;
 import studdybuddy.dataaccess.DataAccess;
 import studdybuddy.dataaccess.DirectDataAccess;
 import studdybuddy.dataaccess.RemoteDataAccess;
@@ -27,29 +19,6 @@ import studdybuddy.json.StuddyBuddiesPersistence;
  * AppController class.
  */
 public class AppController {
-
-  private static final String studdyBuddiesWithTwoStuddyBuddy = """
-    {
-        "StuddyBuddies" : [ {
-          "Name" : "FirstBuddy",
-          "Registrations" : [ {
-            "Date" : "10/10/2022",
-            "Room" : "TestRoom1",
-            "Course" : "TestCourse1",
-            "Start time" : "08:00",
-            "End time" : "12:00"
-          } ]
-        }, {
-          "Name" : "SecondBuddy",
-          "Registrations" : [ {
-            "Date" : "12/12/2022",
-            "Room" : "TestRoom2",
-            "Course" : "TestCourse2",
-            "Start time" : "10:00",
-            "End time" : "14:00"
-          } ]
-        } ]
-    } """;
 
   @FXML
   String userStuddyBuddyPath;
@@ -70,73 +39,7 @@ public class AppController {
   private StuddyBuddies buddies;
 
   /**
-   * Method that first tries to read file from home folder. If StuddyBuddies
-   * object is null then tries to sample studdybuddies from resource instead.
-   *
-   * @return buddies 
-   */
-  private StuddyBuddies getInitialStuddyBuddies() {
-    StuddyBuddies buddies = null;
-
-    if (persistence != null) {
-      try {
-        buddies = persistence.loadStuddyBuddies();
-      } catch (Exception e) {
-        System.err.println("Could not load saved StuddyBuddies");
-      }
-    }
-
-    if (buddies == null) {
-      Reader reader = null;
-      if (sampleStuddyBuddyResource != null) {
-        URL url = getClass().getResource(sampleStuddyBuddyResource);
-        if (url != null) {
-          try {
-            reader = new InputStreamReader(url.openStream(), StandardCharsets.UTF_8);
-          } catch (IOException e) {
-            System.err.println("Could not read from source " + sampleStuddyBuddyResource);
-          }
-        } else {
-          System.err.println("Could not find source: " + sampleStuddyBuddyResource);
-        }
-      }
-      if (reader == null) {
-        reader = new StringReader(studdyBuddiesWithTwoStuddyBuddy);
-      }
-      try {
-        buddies = persistence.readStuddyBuddies(reader);
-      } catch (IOException e) {
-        // this is ingored
-      } finally {
-        try {
-          if (reader != null) {
-            reader.close();
-          }
-        } catch (IOException e) {
-          // this is ingored
-        }
-      }
-    }
-    if (buddies == null) {
-      buddies = new StuddyBuddies();
-      StuddyBuddy buddy = new StuddyBuddy();
-      buddy.setName("Selma");
-      buddy.setPassword("password1");
-      StuddyBuddyRegistration registration = new StuddyBuddyRegistration();
-      LocalDate date1 = LocalDate.of(2021, 11, 11);
-      registration.setDate(date1);
-      registration.setCourse("ITP");
-      registration.setRoom("A3");
-      registration.setStartTime("12:00");
-      registration.setEndTime("14:00");
-      buddy.addRegistration(registration);
-      buddies.addStuddyBuddy(buddy);
-    }
-    return buddies;
-  }
-
-  /**
-   * method for initializing.
+   * Method for initializing.
    */
   @FXML
   public void initialize() {
@@ -152,7 +55,7 @@ public class AppController {
     if (dataAccess == null) {
       this.persistence = new StuddyBuddiesPersistence();
       persistence.setSaveFilePath(userStuddyBuddyPath);
-      DirectDataAccess directAccess = new DirectDataAccess(getInitialStuddyBuddies());
+      DirectDataAccess directAccess = new DirectDataAccess();
       directAccess.setPersistence(persistence);
       dataAccess = directAccess;
     }
