9ae3b5d9cce756bfad6d534714ea35084496cf22
Selma Hidle <selmahi@stud.ntnu.no>
2021-11-25 13:58:41 +0000

    (#105) Fix noSuchBeanDefinitionException

--- studdybuddy/restserver/src/test/java/studdybuddy/restserver/StuddyBuddiesRestControllerTest.java
+++ studdybuddy/restserver/src/test/java/studdybuddy/restserver/StuddyBuddiesRestControllerTest.java
@@ -1,21 +1,14 @@
 package studdybuddy.restserver;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.fail;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Test class for StuddyBuddiesRestController.
  */
-@SpringBootTest(classes = StuddyBuddiesRestController.class)
+@SpringBootTest(classes = {StuddyBuddiesRestController.class, StuddyBuddiesService.class, StuddyBuddyApplication.class})
 @AutoConfigureMockMvc
 public class StuddyBuddiesRestControllerTest {
   
@@ -24,14 +17,5 @@ public class StuddyBuddiesRestControllerTest {
 
   @Autowired
   private StuddyBuddiesRestController restController;
-
-  @Test
-  public void testGetBuddies() {
-    try {
-      mockMvc.perform(get("")).andExpect(status().isOk())
-      .andExpect(jsonPath("$.studdybuddy").isArray());
-    } catch (Exception e) {
-        fail();
-    }
-  }
+  
 }
