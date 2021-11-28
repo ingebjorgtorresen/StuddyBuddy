c91037f65e142476de718b777dc9148e21d83acd
Tuva Langedal Djupvik <tuvald@stud.ntnu.no>
2021-11-28 13:48:05 +0000

    (#103) added comment

--- studdybuddy/core/src/test/java/studdybuddy/core/StuddyBuddyValidationTest.java
+++ studdybuddy/core/src/test/java/studdybuddy/core/StuddyBuddyValidationTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Class for testing StuddyBuddyValidation
+ */
 public class StuddyBuddyValidationTest {
 
   /**
